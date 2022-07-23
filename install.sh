f9cfd287f3b12a18e0342f6bf22b37c4623fed03
DESKTOP-7DMSQ99\AgentSmit <pnz.r00t@gmail.com>
2022-07-23 18:03:22 +0300

    Init commit

---
+++ install.sh
@@ -0,0 +1,19 @@
+git clone https://github.com/clovaai/CRAFT-pytorch.git
+git clone https://github.com/clovaai/deep-text-recognition-benchmark
+
+# create dir for models
+mkdir weights
+# craft main
+wget -O weights/craft_mlt_25k.pth https://drive.google.com/uc?id=1Jk4eGD7crsqCCg9C9VjCLkMN3ze8kutZ&export=download
+# craft refiner
+wget -O weights/craft_refiner_CTW1500.pth https://drive.google.com/uc?id=1XSaFwBkOaFOdtk4Ane3DFyJGPRw6v5bO&export=download
+# deep text recongnition models
+wget -O weights/TPS-ResNet-BiLSTM-Attn.pth https://www.dropbox.com/sh/j3xmli4di1zuv3s/AADbTu4LF-nMUBmC43_RQ8OGa/TPS-ResNet-BiLSTM-Attn.pth?dl=1
+wget -O weights/TPS-ResNet-BiLSTM-CTC.pth https://www.dropbox.com/sh/j3xmli4di1zuv3s/AAB0X-sX05-0psb4uXWPYSmza/TPS-ResNet-BiLSTM-CTC.pth?dl=1
+
+
+# Result images folders, probably it can, be not necessary
+mkdir ./results_images/
+
+echo ">>>>>I install all.<<<<<"
+echo "Don't forget run command for installing libs \"pil install -r requirements.txt\""
